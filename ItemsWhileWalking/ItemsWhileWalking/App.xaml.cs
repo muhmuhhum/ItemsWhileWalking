bf4cad24a085cee0cc6d8e7e7af5fd53cac68790
Tobias Müller <muhhum@live.de>
2019-07-07 14:54:45 +0200

    Projektdateien hinzufügen.

---
+++ ItemsWhileWalking/ItemsWhileWalking/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ItemsWhileWalking
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+#if DEBUG
+            HotReloader.Current.Run(this);
+            //optionaly you may specify EXTENSION's IP (ExtensionIpAddress) 
+            //in case your PC/laptop and device are in different subnets
+            //e.g. 10.10.102.16 AND 10.10.9.12
+#endif
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
